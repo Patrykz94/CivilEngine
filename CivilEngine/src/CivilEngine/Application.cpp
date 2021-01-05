be53eb0e27a1989c52f0956e04840de71f154176
Patryk Zawierucha <zawpat@googlemail.com>
2021-01-05 20:28:09 +0000

    Add an Event system

--- CivilEngine/src/CivilEngine/Application.cpp
+++ CivilEngine/src/CivilEngine/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "CivilEngine/Events/ApplicationEvent.h"
+#include "CivilEngine/Log.h"
+
 namespace Civil {
 
 	Application::Application()
@@ -12,6 +15,16 @@ namespace Civil {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			CE_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			CE_TRACE(e);
+		}
+
 		while (true);
 	}
 
--- premake5.lua
+++ premake5.lua
@@ -26,6 +26,7 @@ project "CivilEngine"
 
 	includedirs
 	{
+		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include"
 	}
 
